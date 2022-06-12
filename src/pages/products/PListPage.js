4b2492863b625df196b37d847d0fd6030ea9e499
kangckck23@gmail.com <kangckck23@gmail.com>
2022-06-13 00:26:52 +0900

    Axios React Router page move

--- src/pages/products/PListPage.js
+++ src/pages/products/PListPage.js
@@ -1,49 +1,93 @@
-import React, {useEffect} from 'react';
-import { useSearchParams } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import { useSearchParams, useNavigate  } from "react-router-dom";
 import axios from "axios";
 
 
-const parameterInit = {
+const initPageParam = {
     page:1,
     size:10
+}
+
+const initState = {
+    dtoList:[],
+    page:0,
+    size:0,
+    start:0,
+    end:0,
+    total:0,
+    prev:false,
+    next:false
 }
 
 
 const getServerGuestTodo = async (requestParam) => {
 
+    console.log('getServerGuestTodo....')
+
     const header = { 'content-type': 'application/x-www-form-urlencoded' }
 
     const res = await axios.get("http://localhost:8080/guest/todo/list", {headers:header, params:requestParam})
-
-    console.log(res.data)
 
     return res.data
 
 }
 
+
+const makeList = (start, end) => {
+
+    const result = Array.from({length:(end-start +1)}, (key,idx) => idx+1)
+    return result
+
+}
+
+
 const PListPage = () => {
 
     let [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate();
 
-    console.log("searchParams")
-    console.log(searchParams.get("page"))
+    const [serverData, setServerData] = useState(initState)
+
     console.log("--------------------")
-
-    const pageRequest = {page: searchParams.get("page")| parameterInit.page, size: searchParams.get("size")|parameterInit.size}
-
-    console.log(pageRequest)
+    console.log(serverData)
 
     useEffect(() => {
 
         console.log("useEffect....")
+
+        const pageRequest = {page: searchParams.get("page")||1, size:searchParams.get("size")||10}
+
+
         getServerGuestTodo(pageRequest)
+            .then(newData =>{
+                setServerData({...newData})
+                navigate(`../list?page=${pageRequest.page}&size=${pageRequest.size}`, { replace: true });
+            })
 
+    },[searchParams.get("page")])
 
-    },[])
+    const movePage = (num) => {
+
+        alert(`../list?page=${num}&size=${searchParams.get("size")}`)
+
+        navigate(`../list?page=${num}&size=${searchParams.get("size")}`, { push: true });
+    }
 
     return (
         <div>
             <h1>Product List Page</h1>
+            <h2>{serverData.total}</h2>
+            <ul>
+                {serverData.dtoList.map(dto => <li key={dto.tno}>{dto.title}</li>)}
+            </ul>
+            <div>
+                <ul>
+                    {serverData.prev?<li>Prev</li>:<></>}
+                    {makeList(serverData.start,serverData.end).map((num, ki) => <li key={ki} onClick={(e) => movePage(num)}><b>{num}</b></li>) }
+                    {serverData.next?<li>Next</li>:<></>}
+                </ul>
+
+            </div>
         </div>
     );
 };
